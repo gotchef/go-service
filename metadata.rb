afa82e2df2e9fbc23d5e5d5a38e834853ab4f8c0
Franklin Wise <franklin@krave.io>
2015-03-09 14:51:27 -0700

    rename

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name             'go-chef'
+name             'go-service'
 maintainer       'Franklin Wise'
 maintainer_email ''
 license          'MIT License'
-description      'utiltiies for installing go apps with chef'
+description      'utiltiies for installing go services with chef'
 version          '0.1.1'
 
 
