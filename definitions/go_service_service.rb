9e222245a553dd3324fd0465d069e11783f5ea25
Franklin Wise <franklin@krave.io>
2015-03-18 16:22:45 -0700

    user

--- definitions/go_service_service.rb
+++ definitions/go_service_service.rb
@@ -1,5 +1,5 @@
 
-define :go_service_service, :service_name => '', :run_file => '', :run_args => '', :options => {}, :install_root => '/opt', :log_root => '/var/log', :timeout => 60 do
+define :go_service_service, :service_name => '', :run_file => '', :run_args => '', :options => {}, :user => '', :group => '', :install_root => '/opt', :log_root => '/var/log', :timeout => 60 do
 	include_recipe 'runit'
 	
 	options = params[:options]
@@ -7,18 +7,22 @@ define :go_service_service, :service_name => '', :run_file => '', :run_args => '
 	run_args = params[:run_args]
 	log_root = params[:log_root]
 	timeout = params[:timeout]
-
 	service_name = params[:service_name]
-	run_file = params[:run_file]
+	user = params[:user]
+	group = params[:group]
 
+	user = service_name unless !user.to_s.empty?
+	group = user unless !group.to_s.empty?
+
+	run_file = params[:run_file]
 	run_file = service_name unless !run_file.to_s.empty?
 
 	executable_path="#{install_root}/#{service_name}/current/bin/#{run_file} #{run_args}" # all json in folder
 	
 	log_dir = "#{log_root}/#{service_name}"
 	directory log_dir do
-		group service[:group]
-		owner service[:user]
+		group group
+		owner user
 		mode "0775"
 		action :create
 		recursive true
@@ -26,7 +30,7 @@ define :go_service_service, :service_name => '', :run_file => '', :run_args => '
 	
 	service_options = {
 		:exec => executable_path,
-		:user => service[:user],
+		:user => user,
 		:log_dir => log_dir
 	}.merge(options)
 
