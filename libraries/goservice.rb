39f4e0c094ea10052563c257e9d9a6d00156d5e6
Franklin Wise <franklin@krave.io>
2015-03-23 21:19:47 -0700

    corrected service path

--- libraries/goservice.rb
+++ libraries/goservice.rb
@@ -2,16 +2,12 @@
 class Chef
 	module GoService
 		class << self
-		  def active_binary(node)
-		    File.join(node['consul']['install_dir'], 'consul')
-		  end
-		
 		  def cached_archive(node)
 		    File.join(Chef::Config[:file_cache_path], File.basename(remote_url(node)))
 		  end
 
-		  def config_root(node)
-			config_root = node[:service][:config_root]
+		  def config_root(service)
+			config_root = service[:config_root]
 			config_root = "/etc" unless !config_root.to_s.empty?
 			return config_root
 		  end
